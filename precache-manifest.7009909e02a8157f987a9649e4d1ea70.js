dc2e6cceeed9f582b9503ac53b09af3aae85bc8f
Imran Ahmad Shahid <imranahmadshahid1@gmail.com>
2020-07-12 03:54:59 +0300

    Updates

---
+++ precache-manifest.7009909e02a8157f987a9649e4d1ea70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bcb006704e0e332a6164ef28629fe9",
+    "url": "/ReactTestApplication20200707/index.html"
+  },
+  {
+    "revision": "4a097a5aa14fd118bd24",
+    "url": "/ReactTestApplication20200707/static/css/main.74462eda.chunk.css"
+  },
+  {
+    "revision": "c6de2034b8b3023c7249",
+    "url": "/ReactTestApplication20200707/static/js/2.d5826b4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTestApplication20200707/static/js/2.d5826b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a097a5aa14fd118bd24",
+    "url": "/ReactTestApplication20200707/static/js/main.24dbe92b.chunk.js"
+  },
+  {
+    "revision": "47cc873b9fbd909fffd7",
+    "url": "/ReactTestApplication20200707/static/js/runtime-main.6c04e744.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactTestApplication20200707/precache-manifest.7009909e02a8157f987a9649e4d1ea70.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactTestApplication20200707/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
